8f8c0b644a0a6982675719d256a13704a9d8c5b9
Hagane26 <haganeizzy@gmail.com>
2023-05-31 14:52:17 +0700

    update ke 2

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -3,7 +3,7 @@
 <body>
 
     <header>
-        <h2 class="logo"> INTEL </h2>
+        <h2 class="logo"> INTEEL </h2>
         <nav class="navigation">
             <a href="#">Home</a>
             <a href="#">Detail</a>
@@ -12,22 +12,77 @@
     </header>
 
     <div class="horn-alert">
+        <marquee>Test</marquee>
     </div>
 
     <div class="row">
         <div class="column">
             <table border="1px" width="100%" height="100%">
                 <tr>
-                    <td>
-                        <center>
-                            <img src="{{ asset('img/img1.jpg') }}" height="250px" width="250px">
-                        </center>
+                    <td width="300px" height="300px">
+                        <div class="tron">
+                            <iframe width="100%" height="250"
+                                src="https://www.youtube.com/embed/rrpUYPT1crM?autoplay=1" title="YouTube video player"
+                                frameborder="0" allowfullscreen></iframe>
+                        </div>
                     </td>
                 </tr>
 
                 <tr>
                     <td>
-
+                        <h2> Jadwal Kuliah </h2>
+                        <table class="table-jadwal-kuliah" border="1px">
+                            <tr>
+                                <th width="40px"></th>
+                                <th>Senin</th>
+                                <th>Selasa</th>
+                                <th>Rabu</th>
+                                <th>Kamis</th>
+                                <th>Jumat</th>
+                                <th>Sabtu</th>
+                                <th>Minggu</th>
+                            </tr>
+                            <tr>
+                                <th>Pagi</th>
+                                <td></td>
+                                <td></td>
+                                <td></td>
+                                <td></td>
+                                <td></td>
+                                <td></td>
+                                <td></td>
+                            </tr>
+                            <tr>
+                                <th>Siang</th>
+                                <td></td>
+                                <td></td>
+                                <td></td>
+                                <td></td>
+                                <td></td>
+                                <td></td>
+                                <td></td>
+                            </tr>
+                            <tr>
+                                <th>Siang</th>
+                                <td></td>
+                                <td></td>
+                                <td></td>
+                                <td></td>
+                                <td></td>
+                                <td></td>
+                                <td></td>
+                            </tr>
+                            <tr>
+                                <th>Sore</th>
+                                <td></td>
+                                <td></td>
+                                <td></td>
+                                <td></td>
+                                <td></td>
+                                <td></td>
+                                <td></td>
+                            </tr>
+                        </table>
                     </td>
                 </tr>
             </table>
@@ -38,59 +93,6 @@
         </div>
 
     </div>
-
-    <table class="table-jadwal-kuliah" border="1px">
-        <tr>
-            <th width="40px"></th>
-            <th>Senin</th>
-            <th>Selasa</th>
-            <th>Rabu</th>
-            <th>Kamis</th>
-            <th>Jumat</th>
-            <th>Sabtu</th>
-            <th>Minggu</th>
-        </tr>
-        <tr>
-            <th>Pagi</th>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
-        </tr>
-        <tr>
-            <th>Siang</th>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
-        </tr>
-        <tr>
-            <th>Siang</th>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
-        </tr>
-        <tr>
-            <th>Sore</th>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
-        </tr>
-    </table>
 
     <div class="side-nav">
         <p>Dosen Yang Ada di Prodi</p>
